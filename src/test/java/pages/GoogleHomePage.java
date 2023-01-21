8b6e8371d538cbc9c7270a734100c6dfee575998
Yusuf Renkoglu <yusuf@renkoglu.com>
2023-01-21 18:15:21 +0200

    Cucumber Framework v1

---
+++ src/test/java/pages/GoogleHomePage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleHomePage {
+    public GoogleHomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "(//div[@class='QS5gu sy4vM'])[2]")
+    public WebElement acceptCookiesButton;
+
+    @FindBy(css = "[name=q]")
+    public WebElement searchBoxInput;
+
+    @FindBy(id = "result-stats")
+    public WebElement resultText;
+}
